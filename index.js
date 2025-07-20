88e2745dcf923d66eabe7ee9941390fb60bd84d7
Arman-2107046 <armanr.rafi@gmail.com>
2025-07-20 12:57:52 +0600

    updated frontend url

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://cotton-world-sourcing-frontend.netlify.app/', // frontend URL
+}));
 app.use(express.json());
 
 // Setup nodemailer transporter
