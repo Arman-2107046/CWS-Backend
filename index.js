2e1a03635b4203805a790c33a8ab94363047c95d
Arman-2107046 <armanr.rafi@gmail.com>
2025-07-20 20:27:55 +0600

    final update done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
     cors({
-        origin: "https://cotton-world-sourcing-frontend.netlify.app",
+        origin: "https://www.cottonworldsourcing.com",
         credentials: true,
     })
 );
